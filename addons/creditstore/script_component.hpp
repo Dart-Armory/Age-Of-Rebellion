c59b89d7a8d531a865439a087bb55a51e39fbabe
DartRuffian <skulldude327@gmail.com>
2024-04-15 16:14:13 -0500

    Start of addon

---
+++ addons/creditstore/script_component.hpp
@@ -0,0 +1,8 @@
+#define COMPONENT creditStore
+#define COMPONENT_BEAUTIFIED Credit Store
+#include "\DA\AOR\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#include "\DA\AOR\addons\main\script_macros.hpp"
